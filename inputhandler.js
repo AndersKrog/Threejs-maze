19b2c2b8112f9c5544ee8d14cc4d35957d2fbd94
Borgerskabet <70210262+Borgerskabet@users.noreply.github.com>
2021-05-04 07:43:15 +0200

    Add files via upload

--- inputhandler.js
+++ inputhandler.js
@@ -19,8 +19,6 @@ export default class InputHandler{
 			if (event.keyCode == 70	){		// f
 				visitor.shiftFloor();
 			}		
-
-
 		});
 			
 		document.addEventListener("keyup", event => {
@@ -35,3 +33,26 @@ export default class InputHandler{
 		});	
 	}	
 }
+
+
+// pointer lock API
+//https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+
+/*
+canvas.requestPointerLock = canvas.requestPointerLock ||
+                            canvas.mozRequestPointerLock;
+
+document.exitPointerLock = document.exitPointerLock ||
+                           document.mozExitPointerLock;
+
+canvas.onclick = function() {
+  canvas.requestPointerLock();
+  // skal eventuelt slås fra, hvis musen ikke er fanget
+
+	
+	//document.addEventListener("mousemove", function () {visitor.updatePosition(e,camera_FP)}, true);
+	//hvis man skal passe et argument sammen med eventet kan det gøres således:
+	
+	document.addEventListener("mousemove", (e) => {visitor.updatePosition(e,camera_FP)}, true);
+};
+*/
